6fa9c6d6b322ea8e63e890b25a5108f0d09b2295
cristiansep <cristiansepulvedamendez@gmail.com>
2019-12-23 23:43:35 -0300

    Aplicación lista

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { async } from '@angular/core/testing';
+import { Lista } from '../../models/lista.model';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(
+    public deseosService: DeseosService,
+    private router: Router,
+    private alertControl: AlertController
+    ) {}
+
+  async agregarLista() {
+
+    const alert = await this.alertControl.create({
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            console.log('data');
+            if (data.titulo.length === 0) {
+              return;
+            }
+
+            const listaId = this.deseosService.crearLista(data.titulo);
+            // Craer lista
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+}
